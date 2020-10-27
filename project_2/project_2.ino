9d26e44db4d356b945dca9074a323079f37a0903
LukeBrandon <lukebrandon68@gmail.com>
2020-10-27 14:13:35 -0500

    working project 2

--- project_2/project_2.ino
+++ project_2/project_2.ino
@@ -1,21 +1,24 @@
+#include <SPI.h>
+#include <WiFi101.h>
 #include <Servo.h>
 #include <LiquidCrystal.h>
 #include <PubSubClient.h>
-#include <SPI.h>
-#include <WiFi101.h>
 
 Servo servo;
-LiquidCrystal lcd(0,1,5,4,3,2);
+LiquidCrystal lcd(0, 1, 5, 4, 3, 2);
+char* testMessage = "TESTING...";
 
 //-------------------------------------
 WiFiClient wifiClient;
 int status = WL_IDLE_STATUS;
-char* ssid = "ThePromisedLan 2.4G";
-char* password = "sucksformoses";
+char* ssid = "ThePromisedLan2.4G";
+char* password ="sucksformoses";
+long int lastWifiAttempt = 0;
 
-PubSubClient client(wifiClient);
+PubSubClient mqttClient(wifiClient);
 char* username = "lhbrando";
-char* mqtt_server = "thor.csce.uark.edu";
+char* mqtt_server = "broker.hivemq.com";
+//char* mqtt_server = "broker.mqttdashboarmmd.com";
 char* topic_lcd = "uark/csce5013/lhbrando/lcd";
 char* topic_phrase = "uark/csce5013/lhbrando/phrase";
 char* topic_angles = "uark/csce5013/lhbrando/angles";
@@ -26,87 +29,78 @@ const int SERVO_PIN = 9;
 const int SERVO_ANGLE_MULT = 12;
 long timeSinceUpdated = 0;
 
-int angles[36];
-String decodedMessage;
+int const angles_array_size = 36;
+int angles[angles_array_size];
+String decodedMessage = "";
 
 // Initializes all the things
-void setup(){
+void setup() {
   delay(1000);
-  Serial.begin(115200);
-  Serial.println("Serial has begun");
-//  while (!Serial) {
-//    ;
-// }/
-  
+  Serial.begin(9600);
+  while(!Serial){
+    //do nothing
+  }
+
   servo.attach(SERVO_PIN);
   servo.write(0);
-  lcd.begin(16,2);
-  clearAnglesArray();
-  decodedMessage = "";
-
-  Serial.println("Starting");
+  lcd.begin(16, 2);
+  lcd.print(testMessage);
+  
+  resetAnglesArray();
+  
   setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(messageReceivedCallback);
   
-  // Connect to MQTT Server
-  if (client.connect("Luke'sMKR1000")) {
-    Serial.println("Connected, now subscribing");
-      boolean lcdSuccess = client.subscribe(topic_lcd);
-      boolean phraseSuccess = client.subscribe(topic_phrase);
-      boolean anglesSuccess = client.subscribe(topic_angles);
-      Serial.print("Phrase: ");
-      Serial.println(phraseSuccess);
-      Serial.print("Lcd: ");
-      Serial.println(lcdSuccess);
-      Serial.print("Angles: ");
-      Serial.println(anglesSuccess);
+  mqttClient.setServer(mqtt_server, 1883);
+  mqttClient.setCallback(messageReceivedCallback);
 
-  } 
-  else {
-    Serial.println("Connection to Thor failed ");
-  }
-  
+  // Connect to MQTT Server
+  reconnect();
+
   //Start off communication with server by sending HELLO
   publishPhrase(init_communication_message);
-
 }
 
-// Reconnect to client
+// Reconnect to mqttClient
 void reconnect() {
-  while (!client.connected()) {
-    
+  Serial.print("WiFi Connected: ");
+  Serial.println(WiFi.status() == WL_CONNECTED);
+
+  while (!mqttClient.connected()) {
+
     Serial.print("Attempting MQTT connection...");
-    
+
     // Attempt to connect
-    if (client.connect(username)) {
-      Serial.println("connected");
-      boolean phraseSuccess = client.subscribe(topic_phrase);
-      boolean lcdSuccess = client.subscribe(topic_lcd);
-      boolean anglesSuccess = client.subscribe(topic_angles);
-      Serial.print("Subbed to Phrase: ");
-      Serial.println(phraseSuccess);
+    if (mqttClient.connect(username)) {
+      Serial.println("Connected. Now subscribing...");
+      boolean lcdSuccess = mqttClient.subscribe(topic_lcd);
+      boolean phraseSuccess = mqttClient.subscribe(topic_phrase);
+      boolean anglesSuccess = mqttClient.subscribe(topic_angles);
       Serial.print("Subbed to Lcd: ");
       Serial.println(lcdSuccess);
+      Serial.print("Subbed to Phrase: ");
+      Serial.println(phraseSuccess);
       Serial.print("Subbed to Angles: ");
       Serial.println(anglesSuccess);
 
     } else {
-      Serial.println("Failed, try again in 5 seconds");
+      Serial.println("Connection failed, try again in 5 seconds");
       // Wait 5 seconds before retrying
       delay(5000);
     }
   }
 }
 
-void loop(){
-  if (!client.connected())  // Reconnect if connection is lost
-  {
+void loop() {
+  delay(1000);
+
+  // Reconnect if connection is lost
+  if (!mqttClient.connected()) {
     reconnect();
   }
-  client.loop();
+
+  mqttClient.loop();
+
   delay(100);
-  
 }
 
 void messageReceivedCallback(char* topic, byte* message, unsigned int length) {
@@ -124,97 +118,149 @@ void messageReceivedCallback(char* topic, byte* message, unsigned int length) {
 
   // Determine what to do with the message
   if (String(topic) == topic_phrase) {
-      // Got the secret phrase
-  } else if (String(topic) == topic_angles){
-      // Decode the angles 
-      decodeAngles(messageTemp);
-      displayHex();
-  } else if (String(topic) == topic_lcd){
-      lcdDisplayMessage(messageTemp);
+    
+    // Got the secret phrase
+  } else if (String(topic) == topic_angles) {
+    
+    // Decode the angles
+    decodeAngles(messageTemp);
+    displayHex();
+    hexToAscii();
+
+    // Convert the decoded message into a char array
+    char phrase[decodedMessage.length()+1];
+    decodedMessage.toCharArray(phrase, decodedMessage.length()+1);
+    
+    publishPhrase(phrase);
+  } else if (String(topic) == topic_lcd) {
+    
+    lcdDisplayMessage(messageTemp);
   } else {
     Serial.println("Whoops! I received a message from a topic that I am not supposed to be subscribed to.");
   }
 }
 
-
-// This method is responsible for handling the moving of the servo (Pathfinder) when a message is received
-void displayHex(){  
-  for(int i = 0; i < sizeof(angles);i++){
-    Serial.print(angles[i]);
-    Serial.print(",");
-  }
-  
-  for(int i=0; i < sizeof(angles); i++){
-      if(angles[i] == -1)
-        return;
-
-      servo.write(angles[i]);
-      Serial.print("moving servo to ");
-      Serial.println(angles[i]);
-
-      delay(500);
-  }
-}
-
 // Displays the given string onto the LCD display
-void lcdDisplayMessage(String message){
+void lcdDisplayMessage(String message) {
+  Serial.print("Printing \"");
+  Serial.print(message);
+  Serial.println("\" to LCD");
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print(message);
 }
 
-//Helper function for decoding the comma separated angles sent from server into an array of integers
-void decodeAngles(String message){
-  clearAnglesArray();
+// This method is responsible for handling the moving of the servo (Pathfinder) when a message is received
+void displayHex() {
+  for (int i = 0; i < angles_array_size; i++) {
+    Serial.print(angles[i]);
+    Serial.print(",");
+  }
+
+  Serial.println("");
+
+  for (int i = 0; i < angles_array_size; i++) {
+    if (angles[i] == -1)
+      return;
+
+    servo.write(angles[i]);
+    Serial.print("moving servo to ");
+    Serial.println(angles[i]);
+
+    delay(500);
+  }
+}
+
+// This function uses the angles that were went from broker and converted to ascii
+void hexToAscii(){
+  // Each ascii character is made up of 2 hex values
+  // We get these hex values by dividing the angle by 12 (16 evenly spaced over 180 degrees)  
+  decodedMessage = "";
+  int firstHex;
+  int secondHex;
+  int decimalVal;
+  
+  for(int i=0; i < angles_array_size; i+=2){
+    if(angles[i]!= -1){ 
+      Serial.print(",");
+      Serial.print(angles[i]);
+      Serial.print(",");
+      Serial.print(angles[i+1]);
+       
+      //reset values
+      firstHex = 0;
+      secondHex = 0;
+      decimalVal = 0;
+        
+      firstHex = angles[i]/12;
+      secondHex = angles[i+1]/12;
+
+      // Interpret the hex values into an ASCII decimal value
+      decimalVal = (firstHex * 16) + (secondHex);
+        
+      decodedMessage.concat(char(decimalVal));
+      Serial.println(decodedMessage);  
+    }  
+  }
+  
+  Serial.println("Finished decoding message");
+} 
+
+// Helper function for decoding the comma separated angles sent from server into an array of integers
+void decodeAngles(String message) {
+  resetAnglesArray();
 
   int currentAngleNum = 0;
   String currentValue = "";
-  
-  for(int i=0; i<message.length();i++){
+
+  for (int i = 0; i <= message.length(); i++) {
+    if(i == message.length()){
+       angles[currentAngleNum] = currentValue.toInt();
+       return;
+    }
+    
     // Ignore spaces
-    if(message[i] == ' ')
+    if (message[i] == ' ')
       continue;
-        
-    if(message[i] == ',' || i == sizeof(message)-1){
+
+    if (message[i] == ',' || i == message.length()) {
       angles[currentAngleNum] = currentValue.toInt();
+      //Serial.println(currentValue);
       currentValue = "";
       currentAngleNum++;
-      Serial.println(currentValue);
     } else {
       currentValue.concat(message[i]);
     }
   }
-  
+
 }
 
-void publishPhrase(char phrase[]){
-  // Topic:  uark/csce5013/lhbrando/phrase
-  // Publish the completed phrase here
-  boolean success = client.publish(topic_phrase, phrase);
+// Publishes whatever is passed in to the phrase topic
+void publishPhrase(char phrase[]) {
+  Serial.print("Publishing the phrase: ");
+  Serial.println(phrase);
+  boolean success = mqttClient.publish(topic_phrase, phrase);
   Serial.print("Pub success: ");
   Serial.println(success);
 }
 
+// Connect to wifi
 void setup_wifi() {
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
   while (status != WL_CONNECTED) {
-    status = WiFi.begin(ssid, password);
+      Serial.print("Attempting connection");
+      status = WiFi.begin(ssid, password);
+      Serial.println(".");
   }
 
   Serial.println("");
   Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 }
 
 // Makes all entries in the angles array -1
-void clearAnglesArray(){
-  for(int i = 0; i < sizeof(angles); i++){
+void resetAnglesArray() {
+  for (int i = 0; i < angles_array_size; i++) {
     angles[i] = -1;
   }
 }
